47b5d3d0a28b7c247e31c6a5dfe3f79d3ac784c0
beobsung <beobsung1@gmail.com>
2021-10-28 21:50:57 +0900

    Update

--- Algo_Quiz/Leet_2.모범답_3Sum.cpp
+++ Algo_Quiz/Leet_2.모범답_3Sum.cpp
@@ -1,41 +1,46 @@
 #include <vector>
+#include <set>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int> &nums)
 {
-    vector<vector<int>> rs;
-    sort(nums.begin(), nums.end());
-    int i, j, k;
-    int n = nums.size();
-    for (i = 0; i < n - 2; i++)
+    set<vector<int>> rs;            //set을 이용해서 중복제거
+    sort(nums.begin(), nums.end()); //정렬하고
+    int sz = nums.size();
+
+    for (int i = 0; i < sz - 2; i++)
     {
-        j = i + 1;
-        k = n - 1;
-        while (j < k)
+        int front = i + 1; //j는 앞
+        int back = sz - 1; //k는 맨뒤
+
+        while (front < back)
         {
-            int sum = nums[i] + nums[j] + nums[k];
+            int sum = nums[i] + nums[front] + nums[back];
             if (sum == 0)
             {
                 vector<int> tmp(3);
                 tmp[0] = nums[i];
-                tmp[1] = nums[j];
-                tmp[2] = nums[k];
-                rs.push_back(tmp);
-                j++;
-                k--;
+                tmp[1] = nums[front];
+                tmp[2] = nums[back];
+                rs.insert(tmp);
+
+                front++;
+                back--;
             }
+            //sum을 통해서 이렇게 접근하는 것이 핵심
             else if (sum > 0)
             {
-                k--;
+                back--;
             }
             else if (sum < 0)
             {
-                j++;
+                front++;
             }
         }
     }
+
     vector<vector<int>> result(rs.begin(), rs.end());
     return result;
 }
