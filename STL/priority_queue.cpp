c59fcc6ca4c09380825829d6441259a08507ebb0
Luckoo <beobsung1@gmail.com>
2022-03-13 14:59:13 +0900

    Update

--- STL/priority_queue.cpp
+++ STL/priority_queue.cpp
@@ -258,3 +258,43 @@ after pop_heap, v1: 2 3 4 9 5 1
 former top element: 1
 after removing the former top element, v1: 2 3 4 9 5
 */
+
+//////////////////////////////////////////////////////////////////////////
+//priority_queue_pair
+#include <queue>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void print_queue(T q)
+{ // NB: pass by value so the print uses a copy
+    while (!q.empty())
+    {
+        cout << q.top() << ' ';
+        q.pop();
+    }
+    cout << '\n';
+}
+
+int main()
+{
+    priority_queue<int> q;
+
+    const auto data = {1, 8, 5, 6, 3, 4, 0, 9, 7, 2};
+
+    priority_queue<int, vector<int>, greater<int>> q2(data.begin(), data.end());
+
+    print_queue(q2);
+
+    priority_queue<int, vector<int>, greater<int>> PQ1;
+
+    for (int n : data)
+        PQ1.push(n);
+
+    print_queue(PQ1);
+
+    //이런식으로 pair도 가능
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> PQ;
+    PQ.push(make_pair(1, 2));
+}
