6789e0532e91214fb0b87e49d42a81d410cac434
Luckoo <beobsung1@gmail.com>
2022-04-28 21:59:51 +0900

    PROG_23

--- programmers/PROG_24_1.문자열압축.cpp
+++ programmers/PROG_24_1.문자열압축.cpp
@@ -3,7 +3,50 @@
 #include <iostream>
 using namespace std;
 
-int solution(string s) {
-    int answer = 0;
+int solution(string s)
+{
+    int answer = 99999999;
+    vector<string> vec;
+    for (int i = 0; i < s.size(); i += 2)
+    {
+        vec.push_back(s.substr(i, 1));
+    }
+    string word = vec[0];
+    string temp_str = "";
+    int cnt = 1;
+    for (int i = 1; i < vec.size(); i++)
+    {
+        if (word == vec[i])
+        {
+            cnt += 1;
+        }
+        else
+        {
+            if (cnt != 1)
+            {
+                temp_str = temp_str + (to_string(cnt) + word);
+                cnt = 1;
+            }
+            else
+            {
+                temp_str = temp_str + word;
+                word = vec[i];
+            }
+        }
+
+    }
+    cout<<temp_str<<endl;
+
     return answer;
+}
+
+int main()
+{
+    cout << "ANSWER : 7,9,8,14,17" << endl;
+
+    // cout << solution("aabbaccc") << endl;
+    cout << solution("ababcdcdababcdcd") << endl;
+    // cout << solution("abcabcdede") << endl;
+    // cout << solution("abcabcabcabcdededededede") << endl;
+    // cout << solution("xababcdcdababcdcd") << endl;
 }
