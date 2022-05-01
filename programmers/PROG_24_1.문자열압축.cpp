02953e2a6b13ec8bbdff400d3990a59e956e9e29
prac <beobsung@gmail.com>
2022-05-01 20:36:38 +0900

    PROG_24

--- programmers/PROG_24_1.문자열압축.cpp
+++ programmers/PROG_24_1.문자열압축.cpp
@@ -5,47 +5,61 @@ using namespace std;
 
 int solution(string s)
 {
-    int answer = 99999999;
-    vector<string> vec;
-    int how_many_cut = 1;
-    for (int i = 0; i < s.size(); i += how_many_cut)
+    if (s.size() == 1) //모서리 CASE
     {
-        vec.push_back(s.substr(i, how_many_cut));
+        return 1;
     }
-    string word = vec[0];
-    string temp_str = "";
-    int cnt = 1;
-    for (int i = 1; i < vec.size(); i++)
+
+    int ans = 9999999;
+    for (int i = 1; i <= s.size() / 2; i++)
     {
-        if (word == vec[i])
+        string temp_ans = "";
+        string word = s.substr(0, i);
+        int cnt = 1;
+        for (int j = i; j < s.size(); j += i)
         {
-            cnt += 1;
+            if (word == s.substr(j, i))
+            {
+                cnt += 1;
+            }
+            else
+            {
+                if (cnt == 1)
+                {
+                    temp_ans += word;
+                }
+                else
+                {
+                    temp_ans = temp_ans + to_string(cnt) + word;
+                }
+                cnt = 1;
+                word = s.substr(j, i);
+            }
+        }
+
+        if (cnt == 1)
+        {
+            temp_ans += word;
         }
         else
         {
-            if (cnt != 1)
-            {
-                temp_str = temp_str + (to_string(cnt) + word);
-                cnt = 1;
-            }
-            else if (cnt == 1)
-            {
-                temp_str = temp_str + word;
-            }
-            word = vec[i];
+            temp_ans = temp_ans + to_string(cnt) + word;
         }
-
+        
+        if (temp_ans.size() < ans)
+        {
+            ans = temp_ans.size();
+        }
     }
-    cout<<temp_str<<endl;
 
-    return answer;
+    return ans;
 }
 
 int main()
 {
     cout << "ANSWER : 7,9,8,14,17" << endl;
 
-    cout << solution("aabbaccc") << endl;
+    // cout << solution("aabbaccc") << endl;
     cout << solution("ababcdcdababcdcd") << endl;
     // cout << solution("abcabcdede") << endl;
     // cout << solution("abcabcabcabcdededededede") << endl;
