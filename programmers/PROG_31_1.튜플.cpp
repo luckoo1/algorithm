8be8a1a83b919acdc611a32c0d4997781a56eeb7
Luckoo <beobsung1@gmail.com>
2022-05-10 21:55:04 +0900

    PROG_32

--- programmers/PROG_31_1.튜플.cpp
+++ programmers/PROG_31_1.튜플.cpp
@@ -1,14 +1,87 @@
 #include <string>
 #include <vector>
+#include <unordered_set>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-vector<int> solution(string s)
+bool sort_rule(string a, string b)
 {
-    vector<int> answer;
-    return answer;
+
+    int a_cnt = 0;
+    int b_cnt = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] == ',')
+        {
+            a_cnt++;
+        }
+    }
+    for (int i = 0; i < b.size(); i++)
+    {
+        if (b[i] == ',')
+        {
+            b_cnt++;
+        }
+    }
+
+    if (a_cnt < b_cnt)
+    {
+        return true;
+    }
+    return false;
 }
 
+bool check[100001];
+vector<int> solution(string s)
+{
+    vector<string> str_num_vec;
+    for (int i = 1; i < s.size() - 1; i++)
+    {
+        if (s[i] == '{')
+        {
+            i++;
+            string temp_str = "";
+            while (s[i] != '}')
+            {
+                temp_str.push_back(s[i]);
+                i++;
+            }
+            str_num_vec.push_back(temp_str);
+        }
+    }
+    sort(str_num_vec.begin(), str_num_vec.end(), sort_rule);
+
+    vector<int> answer;
+    for (int i = 0; i < str_num_vec.size(); i++)
+    {
+        string temp_str = "";
+        
+        for (int j = 0; j < str_num_vec[i].size(); j++)
+        {
+            if (str_num_vec[i][j] != ',')
+            {
+                temp_str.push_back(str_num_vec[i][j]);
+            }
+            else
+            {
+                if (check[stoi(temp_str)] == false)
+                {
+                    check[stoi(temp_str)] = true;
+                    answer.push_back(stoi(temp_str));
+                }
+                temp_str.clear();
+            }
+        }
+        if (check[stoi(temp_str)] == false)
+        {
+            check[stoi(temp_str)] = true;
+            answer.push_back(stoi(temp_str));
+        }
+    }
+
+    return answer;
+}
 int main()
 {
     vector<int> a = solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
---
+++ programmers/PROG_32_1.단체사진찍기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+int solution(int n, vector<string> data) {
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    cout<<solution(2,{"N~F=0", "R~T>2"})<<endl;
+    cout<<solution(2,{"M~C<2", "C~M>1"})<<endl;
+}
