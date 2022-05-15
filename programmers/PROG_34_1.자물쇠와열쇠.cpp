399dfa394cd710c2ad60b76244252b4d3eaa9a87
Luckoo <beobsung1@gmail.com>
2022-05-15 22:41:45 +0900

    PROG_34

---
+++ programmers/PROG_34_1.자물쇠와열쇠.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
+    bool answer = true;
+    return answer;
+}
+
+int main()
+{
+    cout<<solution({{0, 0, 0}, {1, 0, 0}, {0, 1, 1}},{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}});
+}
