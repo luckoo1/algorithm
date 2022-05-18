a751eec0844947c6b325d4b2ed04f57557b76c80
prac <beobsung@gmail.com>
2022-05-18 21:50:23 +0900

    PROG_34

--- programmers/PROG_34_1.자물쇠와열쇠.cpp
+++ programmers/PROG_34_1.자물쇠와열쇠.cpp
@@ -3,12 +3,73 @@
 #include <iostream>
 using namespace std;
 
-bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
+void rotate(vector<vector<int>> &key)
+{
+    int N = key.size();
+    vector<vector<int>> temp(N, vector<int>(N));
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            temp[i][j] = key[j][N - i - 1];
+        }
+    }
+    key = temp;
+}
+
+void move_up()
+{
+    
+}
+void move_down()
+{
+
+}
+void move_left()
+{
+
+}
+void move_right()
+{
+
+}
+
+int count_num(vector<vector<int>> map, int num)
+{
+    int N = map.size();
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (map[i][j] == num)
+            {
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+bool check_lock(vector<vector<int>> key, vector<vector<int>> lock)
+{
+    int lock_cnt = count_num(lock, 0); // lock에서 0의수
+    int key_cnt = count_num(key, 1);
+    if (lock_cnt != key_cnt)
+    {
+        return false;
+    }
+}
+
+bool solution(vector<vector<int>> key, vector<vector<int>> lock)
+{
     bool answer = true;
+    rotate(key);
     return answer;
 }
 
 int main()
 {
-    cout<<solution({{0, 0, 0}, {1, 0, 0}, {0, 1, 1}},{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}});
+
+    cout << solution({{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}, {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}});
 }
