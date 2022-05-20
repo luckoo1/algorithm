55bb01f4bcdc8e520f58e6b15732fca72b6f0ec5
prac <beobsung@gmail.com>
2022-05-20 12:48:54 +0900

    PROG 34

--- programmers/PROG_34_1.자물쇠와열쇠.cpp
+++ programmers/PROG_34_1.자물쇠와열쇠.cpp
@@ -3,25 +3,50 @@
 #include <iostream>
 using namespace std;
 
+int key_size;
+int lock_size;
+
 void rotate(vector<vector<int>> &key)
 {
-    int N = key.size();
-    vector<vector<int>> temp(N, vector<int>(N));
-    for (int i = 0; i < N; i++)
+    vector<vector<int>> temp(key_size, vector<int>(key_size));
+    for (int i = 0; i < key_size; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < key_size; j++)
         {
-            temp[i][j] = key[j][N - i - 1];
+            temp[i][j] = key[j][key_size - i - 1];
         }
     }
     key = temp;
 }
 
 
+bool check_lock(vector<vector<int>> &key, vector<vector<int>> &lock, vector<vector<int>> &check_map)
+{
+    for (int i = 0; i < key_size; i++)
+    {
+        for (int j = 0; j < key_size; j++)
+        {
+            check_map[i][j] = key[i][j];
+        }
+    }
+
+    int key_move = key_size + lock_size - 1;
+
+    for (int i = 0; i < key_move; i++)
+    {
+        for (int j = 0; j < key_move; j++)
+        {
+        }
+    }
+}
+
 bool solution(vector<vector<int>> key, vector<vector<int>> lock)
 {
-    int key_size = key.size();
-    int lock_size = lock.size();
+    key_size = key.size();
+    lock_size = lock.size();
+    int check_lock_size = (key_size * 2) + (lock_size - 2);
+
+    vector<vector<int>> check_map(check_lock_size, vector<int>(check_lock_size));
 
     return true;
 }
