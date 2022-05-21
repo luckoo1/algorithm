5d1afd67d268a8c8cdf9ff840dc26754d53346a5
prac <beobsung@gmail.com>
2022-05-21 19:07:11 +0900

    PROG_35

--- programmers/PROG_35_1.불량사용자.cpp
+++ programmers/PROG_35_1.불량사용자.cpp
@@ -7,7 +7,23 @@
 using namespace std;
 
 map<string, vector<string>> hash_map;
+vector<string> temp_ans;
+set<string> ans_set;
+int banned_size;
 int ans;
+
+bool check_unique(vector<string> vec)
+{
+    sort(vec.begin(), vec.end());
+    for (int i = 0; i < vec.size() - 1; i++)
+    {
+        if (vec[i] == vec[i + 1])
+        {
+            return false; //중복임
+        }
+    }
+    return true; //중복아님
+}
 
 bool check_id(string id, string ban)
 {
@@ -29,8 +45,35 @@ bool check_id(string id, string ban)
     return true;
 }
 
+void dfs(int dep, vector<string> &banned_id)
+{
+    if (dep == banned_size)
+    {
+        if (check_unique(temp_ans) == true)
+        {
+            string temp_str = "";
+            sort(temp_ans.begin(),temp_ans.end());
+            for (auto n : temp_ans)
+            {
+                temp_str += n;
+                temp_str += " ";
+            }
+            ans_set.insert(temp_str);
+        }
+        return;
+    }
+    for (int i = 0; i < hash_map[banned_id[dep]].size(); i++)
+    {
+        temp_ans.push_back(hash_map[banned_id[dep]][i]);
+        dfs(dep + 1, banned_id);
+        temp_ans.pop_back();
+    }
+}
+
 int solution(vector<string> user_id, vector<string> banned_id)
 {
+    banned_size = banned_id.size();
+
     for (int i = 0; i < banned_id.size(); i++)
     {
         for (int j = 0; j < user_id.size(); j++)
@@ -41,8 +84,12 @@ int solution(vector<string> user_id, vector<string> banned_id)
             }
         }
     }
+    dfs(0, banned_id);
+    for(auto iter = ans_set.begin();iter!= ans_set.end();iter++)
+    {
+        cout<<*iter<<endl;
+    }
 
-    
 #if 0
     for(auto iter = hash_map.begin();iter!=hash_map.end();iter++)
     {
@@ -58,9 +105,9 @@ int solution(vector<string> user_id, vector<string> banned_id)
 int main()
 {
     ans = 0;
-    cout << solution({"frodo", "fradi", "crodo", "abc123", "frodoc"}, {"fr*d*", "abc1**"}) << endl;
+    // cout << solution({"frodo", "fradi", "crodo", "abc123", "frodoc"}, {"fr*d*", "abc1**"}) << endl;
     cout << solution({"frodo", "fradi", "crodo", "abc123", "frodoc"}, {"*rodo", "*rodo", "******"}) << endl;
-    cout << solution({"frodo", "fradi", "crodo", "abc123", "frodoc"}, {"fr*d*", "*rodo", "******", "******"}) << endl;
+    // cout << solution({"frodo", "fradi", "crodo", "abc123", "frodoc"}, {"fr*d*", "*rodo", "******", "******"}) << endl;
 }
 /*
 ******,abc123
