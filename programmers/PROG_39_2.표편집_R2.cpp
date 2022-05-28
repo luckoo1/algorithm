10ae9bc5f68288c549d66b20b33c3407cf266414
prac <beobsung@gmail.com>
2022-05-28 21:34:23 +0900

    PROG_39

--- programmers/PROG_39_2.표편집_R2.cpp
+++ programmers/PROG_39_2.표편집_R2.cpp
@@ -1,66 +1,66 @@
-#include <iostream>
+#include <string>
 #include <vector>
 #include <set>
-#include <stack>
-#include <string>
+#include <iostream>
 using namespace std;
 
 string solution(int n, int k, vector<string> cmd)
 {
-    string answer(n, 'X');
-    set<int> table; // 균형 이진트리 기반
-    stack<int> deleted;
-
-    for (int i = 0; i < n; ++i)
+    int move;
+    set<int> table;
+    vector<int> history;
+    for (int i = 0; i < n; i++)
     {
         table.insert(i);
     }
-    auto iter = table.find(k);
 
-    for (int i = 0; i < cmd.size(); ++i)
+    auto iter = table.find(k);  //★Point
+
+    for (int i = 0; i < cmd.size(); i++)
     {
-        char command = cmd[i][0];
-
-        if (command == 'D')
-        { // 아래로
-            int down = stoi(cmd[i].substr(2, cmd[i].size() - 2));
-            while (down > 0)
+        if (cmd[i][0] == 'D')
+        {
+            move = stoi(cmd[i].substr(2, cmd.size() - 1));
+            for (int i = 0; i < move; i++)
             {
-                iter = next(iter);
-                down--;
+                iter++; // iter = next(iter);
             }
         }
-        else if (command == 'U')
-        { // 위로
-            int up = stoi(cmd[i].substr(2, cmd[i].size() - 2));
-            while (up > 0)
+        else if (cmd[i][0] == 'U')
+        {
+            move = stoi(cmd[i].substr(2, cmd.size() - 1));
+            for (int i = 0; i < move; i++)
             {
-                iter = prev(iter);
-                up--;
+                iter--; // iter = prev(iter);
             }
         }
-        else if (command == 'C')
-        { // 삭제
-            //현재 포인터가 가리키는 데이터 삭제, 삭제된 데이터 스택에 push
-            deleted.push(*iter);
+        else if (cmd[i][0] == 'C')
+        {
+            history.push_back(*iter);
             iter = table.erase(iter);
-
-            // erase -> 삭제된 데이터 이후의 포인터를 가리킴
-            // 제일 끝 데이터를 삭제 했다면 마지막 데이터를 가리키도록
-            if (iter == table.end())
-                iter = prev(iter);
+            if(iter == table.end())
+            {
+                iter--;
+            }
         }
-        else
-        { // 복구
-            table.insert(deleted.top());
-            deleted.pop();
+        else if (cmd[i][0] == 'Z')
+        {
+            int recover = history[history.size()-1];
+            table.insert(recover);
+            history.pop_back();
         }
     }
+    string answer(n,'X');//★Point
 
-    for (int i : table)
-        answer[i] = 'O'; // 현재 set 에 남아있는 자료만 체크
+    for(int i:table) {answer[i]='O';}
 
     return answer;
+}
+
+int main()
+{
+    cout << solution(8, 2, {"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"}) << endl;
+    cout << solution(8, 2, {"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"}) << endl;
 }
 int main()
 {
