e1a6fecca767a5dfd146b38375230f982178596a
Luckoo <beobsung1@gmail.com>
2022-05-31 16:33:41 +0900

    PROG_43

--- programmers/PROG_42_1.파괴되지않은건물.cpp
+++ programmers/PROG_42_1.파괴되지않은건물.cpp
@@ -1,3 +1,4 @@
+/*효율성 0점*/
 #include <string>
 #include <vector>
 #include <iostream>
@@ -5,12 +6,51 @@ using namespace std;
 
 int solution(vector<vector<int>> board, vector<vector<int>> skill)
 {
+    for (auto skill : skill)
+    {
+        int type = skill[0];
+        int how_many = skill[5];
+        if (type == 1)
+        {
+            for (int i = skill[1]; i <= skill[3]; i++)
+            {
+                for (int j = skill[2]; j <= skill[4]; j++)
+                {
+                    board[i][j] -= how_many;
+                }
+            }
+        }
+        if (type == 2)
+        {
+            for (int i = skill[1]; i <= skill[3]; i++)
+            {
+                for (int j = skill[2]; j <= skill[4]; j++)
+                {
+                    board[i][j] += how_many;
+                }
+            }
+        }
+    }
+
     int answer = 0;
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[i].size(); j++)
+        {
+            if (board[i][j] > 0)
+            {
+                answer++;
+            }
+        }
+    }
+
     return answer;
 }
 
 int main()
 {
+
+#if 1
     cout << solution({{5, 5, 5, 5, 5},
                       {5, 5, 5, 5, 5},
                       {5, 5, 5, 5, 5},
@@ -27,4 +67,5 @@ int main()
                       {1, 0, 0, 1, 1, 2},
                       {2, 2, 0, 2, 0, 100}})
          << endl;
+#endif
 }
---
+++ programmers/PROG_43_1.블록이동하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<vector<int>> board)
+{
+    int answer = 0;
+    return answer;
+}
+int main()
+{
+    cout << solution(
+        {{0, 0, 0, 1, 1},
+         {0, 0, 0, 1, 0},
+         {0, 1, 0, 1, 1},
+         {1, 1, 0, 0, 1},
+         {0, 0, 0, 0, 0}});
+}
