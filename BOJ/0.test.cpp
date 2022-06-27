9d7f2ebf3e7b30f860562ce6332a9bd0d31b70e4
Luckoo <beobsung1@gmail.com>
2022-06-27 20:56:13 +0900

    -

--- BOJ/0.test.cpp
+++ BOJ/0.test.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+struct DATA
+{
+    int r;
+    int c;
+    DATA(int r,int c)
+    {
+        this->r = r;
+        this->c =c;
+    }
+
+};
+bool sort_rule(vector<int> a,vector<int> b)
+{
+    return a.size()>b.size();
+}
 
 int main()
 {
-    
+    vector<vector<int>> vec;
+    vec.push_back({1,2,3});
+    vec.push_back({1,2});
+    vec.push_back({1,2,3,4});
+    sort(vec.begin(),vec.end(),sort_rule);
+
+    for(auto n:vec)
+    {
+        for(int i=0;i<n.size();i++)
+        {
+            cout<<n[i]<<" ";
+        }
+        cout<<endl;
+    }
+
 }
--- programmers/0.test.cpp
+++ programmers/0.test.cpp
@@ -1,45 +1,30 @@
+#include <set>
 #include <string>
-#include <vector>
 #include <algorithm>
 #include <iostream>
 using namespace std;
-int solution(int distance, vector<int> rocks, int n)
-{
-  int answer = 0;
-  sort(rocks.begin(), rocks.end());
-
-  int start = 0;
-  int end = distance;
-  while (start <= end)
-  {
-    int mid = (start + end) / 2;
-    
-    int cnt = 0;
-    int left = 0;
-    for (int i = 0; i < rocks.size(); i++)
-    {
-      if (rocks[i] - left >= mid)
-        left = rocks[i];
-      else
-        cnt++;
-    }
-
-    if (cnt > n)
-    {
-      end = mid - 1;
-    }
-    else
-    {
-      answer = mid;
-      start = mid + 1;
-    }
-    //cout<<mid<<endl;
-  }
-
-  return answer;
-}
 
 int main()
 {
-    cout<<solution(25,	{2, 14, 11, 21, 17},2)<<endl;    
+    //★set초기화
+    set<int,less<int>> setnum;
+
+    //★set에 요소 추가
+    setnum.insert(7);
+    setnum.insert(4);
+    setnum.insert(3);
+    setnum.insert(5);
+    setnum.insert(7);
+
+    //★set size확인
+    cout << "Set size : " << setnum.size() << endl; //중복이 없어서 4가 나온다.
+    //4
+
+    //★set 출력해보기
+    cout << "Set : ";
+    for (set<int>::iterator it = setnum.begin(); it != setnum.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << "\n";
 }
