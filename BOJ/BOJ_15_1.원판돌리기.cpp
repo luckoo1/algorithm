a42e2cb07c16f35096f10079fdfc724fbe453e24
Luckoo <beobsung1@gmail.com>
2022-07-24 22:19:56 +0900

    BOJ 옮기기

---
+++ BOJ/BOJ_15_1.원판돌리기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<int> clock_rotate(int k)
+{
+    for(int i=0;i<k;i++)
+    {
+        
+    }
+
+}
+vector<int> ban_rotate(int k)
+{
+     for(int i=0;i<k;i++)
+    {
+        
+    }   
+}
+
+int main()
+{
+
+}
