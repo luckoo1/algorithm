369d57f819c862a3857458805cc05e985e61d31f
Luckoo <beobsung1@gmail.com>
2022-08-26 22:22:13 +0900

    result

--- Result_test/5.result.cpp
+++ Result_test/5.result.cpp
@@ -5,128 +5,178 @@
 #include <iostream>
 using namespace std;
 
-
+void print_check(vector<vector<bool>> board)
+{
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[0].size(); j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 void print_vec(vector<vector<int>> board)
 {
-    for(int i=0;i<board.size();i++)
+    for (int i = 0; i < board.size(); i++)
     {
-        for(int j=0;j<board[0].size();j++)
+        for (int j = 0; j < board[0].size(); j++)
         {
-            cout<<board[i][j]<<" ";
+            cout << board[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 struct DATA
 {
     int r;
     int c;
-    int d;
-    DATA(int r,int c,int d)
+
+    DATA(int r, int c)
     {
         this->r = r;
         this->c = c;
-        this->d = d;
     }
 };
-int DR[4]={1,-1,0,0};
-int DC[4]={0,0,1,-1};
-vector<vector<int>> answer;
+int DR[4] = {0, 0, 1, -1};
+int DC[4] = {1, -1, 0, 0};
+
 queue<DATA> q;
 
-int bfs(int x, int y, vector<vector<int>> board,int a_x,int a_y)
+vector<vector<int>> solution(int x, int y, vector<vector<int>> board)
 {
+    print_vec(board);
     int r_size = board.size();
     int c_size = board[0].size();
+    vector<vector<int>> dist_map(r_size, vector<int>(c_size, 0));
+    vector<vector<bool>> check(r_size, vector<bool>(c_size, false));
 
-    vector<vector<int>> diff_map(r_size,vector<int>(c_size,0));
-    vector<vector<bool>> check(r_size,vector<bool>(c_size,false));
+    q.push(DATA(x - 1, y - 1));
+    check[x - 1][y - 1] = true;
 
-    q.push(DATA(x,y,0));
-    check[x][y]=true;
-    int score = board[x][y];
-
-    while(!q.empty())
+    while (!q.empty())
     {
         int start_r = q.front().r;
         int start_c = q.front().c;
-        int start_d = q.front().d;
+        int start_dist = dist_map[start_r][start_c];
+        int start_score = board[start_r][start_c];
         q.pop();
-        for(int k=0;k<4;k++)
+
+        for (int k = 0; k < 4; k++)
         {
-            int move_r = start_r+DR[k];
-            int move_c = start_c+DC[k];
-            if(move_r<0||move_c<0||move_r >= r_size||move_c >= c_size)
+            int move_r = start_r + DR[k];
+            int move_c = start_c + DC[k];
+            if (move_r < 0 || move_c < 0 || move_r >= r_size || move_c >= c_size)
                 continue;
-            // if(check[move_r][move_c]==true)
-            //     continue;
-            if(check[move_r][move_c]==true)
+            if (move_r == x - 1 && move_c == y - 1)
+                continue;
+            if (check[move_r][move_c] == true)
             {
-                if(board[move_r][move_c] == board[start_r][start_c]){
-                    // q.push(DATA(move_r,move_c,start_d));
-                    // check[move_r][move_c] = true;
-                    if(diff_map[move_r][move_c]>start_d){
-                        diff_map[move_r][move_c] = start_d;
+                if (board[move_r][move_c] == start_score)
+                {
+                    if (dist_map[move_r][move_c] > start_dist)
+                    {
+                        dist_map[move_r][move_c] = start_dist;
+                    }
+                }
+                else
+                {
+                    if (dist_map[move_r][move_c] > start_dist + 1)
+                    {
+                        dist_map[move_r][move_c] = start_dist + 1;
                     }
                 }
                 continue;
             }
 
+            if (board[move_r][move_c] == start_score)
+            {
+                q.push(DATA(move_r, move_c));
+                dist_map[move_r][move_c] = start_dist;
+            }
             else
             {
-                q.push(DATA(move_r,move_c,start_d+1));
-                check[move_r][move_c] = true;
-                diff_map[move_r][move_c] = start_d+1;   
+                q.push(DATA(move_r, move_c));
+                dist_map[move_r][move_c] = start_dist + 1;
             }
-
+            check[move_r][move_c] = true;
         }
-    }
-    // cout<<x<<","<<y<<endl;
-    // print_vec(diff_map);
 
-    return diff_map[a_x][a_y];
-}
-vector<vector<int>> solution(int x, int y, vector<vector<int>> board) {
-    int r_size = board.size();
-    int c_size = board[0].size();
-    vector<vector<int>> diff_map(r_size,vector<int>(c_size,0));
-    for(int i=0;i<board.size();i++)
-    {
-        for(int j=0;j<board[0].size();j++)
-        {
-            diff_map[i][j]= bfs(i, j, board,x-1,y-1);
-        }
+        // cout << "PRINT" << endl;
+        // print_vec(dist_map);
+        // cout << "=== CHECK ===" << endl;
+        // print_check(check);
+        //cout << endl;
     }
-    map<int,int> hash_map;
-    for(int i=0;i<diff_map.size();i++)
+
+    map<int, int> hash_map;
+    for (int i = 0; i < dist_map.size(); i++)
     {
-        for(int j=0;j<diff_map[0].size();j++)
+        for (int j = 0; j < dist_map[0].size(); j++)
         {
-            hash_map[diff_map[i][j]] +=1;
+            hash_map[dist_map[i][j]] += 1;
         }
     }
     int max_num = 0;
     int big_diff = 0;
-    print_vec(diff_map);
-    for(auto iter = hash_map.begin();iter!=hash_map.end();iter++)
+    for (auto iter = hash_map.begin(); iter != hash_map.end(); iter++)
     {
-        if(max_num<=iter->second)
+        if (max_num <= iter->second)
         {
             max_num = iter->second;
             big_diff = iter->first;
         }
     }
-    for(int i=0;i<diff_map.size();i++)
+
+    // for (auto iter = hash_map.begin(); iter != hash_map.end(); iter++)
+    // {
+    //     cout<<iter->first<<" "<<iter->second<<endl;
+    // }
+    vector<vector<int>> answer;
+    for (int i = 0; i < dist_map.size(); i++)
     {
-        for(int j=0;j<diff_map[0].size();j++)
+        for (int j = 0; j < dist_map[0].size(); j++)
         {
-            if(diff_map[i][j]==big_diff)
-                answer.push_back({i+1,j+1});
+            if (dist_map[i][j] == big_diff)
+                answer.push_back({i + 1, j + 1});
         }
     }
     return answer;
 }
+
+int main()
+{
+    #if 1
+    vector<vector<int>> vec{
+        {5, 4, 3, 4, 4, 4},
+        {1, 3, 2, 3, 3, 5},
+        {2, 3, 4, 5, 5, 1},
+        {2, 2, 3, 4, 4, 3}};
+    vector<vector<int>> ans = solution(3, 2, vec);
+    #endif
+    #if 0
+    vector<vector<int>> vec{
+        {7,9,7,9,7},
+        {9,7,9,7,9},
+        {7,9,7,9,7},
+        {9,7,9,7,9},
+        {7,9,7,9,7}};
+    vector<vector<int>> ans = solution(3, 3, vec);
+    #endif
+
+    // print_vec(vec);
+    // cout << "RESULT" << endl;
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[0].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 /*
+3, 2
 5 4 3 4 4 4
 1 3 2 3 3 5
 2 3 4 5 5 1
---
+++ a.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+g++ test.cpp
+a
