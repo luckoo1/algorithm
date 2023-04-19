60ad51f322dc55275ec5d625ec63d25dab995a6f
Luckoo <beobsung1@gmail.com>
2023-04-19 22:03:42 +0900

    SAVE

--- save.cpp
+++ save.cpp
@@ -1,134 +1,62 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <queue>
+#include <algorithm>
 using namespace std;
-int N, M;
 
-struct DATA
+bool sort_rule(int a,int b)
 {
-    int r;
-    int c;
-    int t;
-    DATA(int r, int c, int t)
-    {
-        this->r = r;
-        this->c = c;
-        this->t = t;
-    }
-};
-
-int DR[4] = {1, -1, 0, 0};
-int DC[4] = {0, 0, 1, -1};
-
-int water_dist[51][51];
-int dist[51][51];
-
-bool no_graph(int r, int c)
-{
-    if (r < 0 || c < 0 || r >= N || c >= M)
+    if(a>b)
         return true;
     return false;
 }
-
 int main()
 {
-    freopen("Input.txt", "r", stdin);
-    cin >> N >> M;
-    vector<string> graph;
-    for (int i = 0; i < N; i++)
+    //freopen("Input.txt", "r", stdin);
+    int N,max_score;
+    cin>>N>>max_score;
+    int cnt_apply,cnt_max,apply_score;
+    vector<int> ans_vec;
+    for(int i=0;i<N;i++)
     {
-        string temp_str;
-        cin >> temp_str;
-        graph.push_back(temp_str);
-    }
-    int final_r;
-    int final_c;
-    queue<DATA> q;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
+        vector<int> apply_score_vec;
+        cin>>cnt_apply>>cnt_max;
+        for(int j=0;j<cnt_apply;j++)
         {
-            if (graph[i][j] == '*')
-            {
-                q.push(DATA(i, j, 0));
-            }
-            if (graph[i][j]=='S')
-            {
-                final_r = i;
-                final_c = j;
-            }
+            cin>>apply_score;
+            apply_score_vec.push_back(apply_score);
         }
-    }
+        sort(apply_score_vec.begin(),apply_score_vec.end(),sort_rule);
 
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < M; j++)
-            if (graph[i][j] == 'D')
-                q.push(DATA(i, j, 1));
+        if(cnt_apply==1 && cnt_max==1)
+        {
+            apply_score_vec.push_back(0);
+            continue;
+        }
+        if(cnt_apply<cnt_max)
+        {
+            ans_vec.push_back(1);
+        }
+        else
+        {
+            ans_vec.push_back(apply_score_vec[cnt_apply-2]);
+        }
+    }
+    // for(auto n:ans_vec){
+    //     cout<<n<<" ";
+    // }
+    // cout<<endl;
+    sort(ans_vec.begin(),ans_vec.end());
 
-    while (!q.empty())
+    int ans = 0;
+    int ans_cnt = 0;
+    for(int i=0;i<ans_vec.size();i++)
     {
-        int now_r = q.front().r;
-        int now_c = q.front().c;
-        int type = q.front().t;
-        q.pop();
-        if (type == 0) // 물
+        ans+=ans_vec[i];
+        if(ans>=max_score)
         {
-            for (int k = 0; k < 4; k++)
-            {
-                int move_r = now_r + DR[k];
-                int move_c = now_c + DC[k];
-                if (no_graph(move_r, move_c))
-                    continue;
-                if (graph[move_r][move_c] == 'X' || graph[move_r][move_c] == 'S')
-                    continue;
-                if (water_dist[move_r][move_c] != 0)
-                    continue;
-                q.push(DATA(move_r, move_c, type));
-                water_dist[move_r][move_c] = water_dist[now_r][now_c] + 1;
-            }
+            break;
         }
-        else if (type == 1)
-        {
-            for (int k = 0; k < 4; k++)
-            {
-                int move_r = now_r + DR[k];
-                int move_c = now_c + DC[k];
-                if (no_graph(move_r, move_c))
-                    continue;
-                if (graph[move_r][move_c] == 'X')
-                    continue;
-                if (dist[move_r][move_c] != 0)
-                    continue;
-                if (water_dist[move_r][move_c] != 0)
-                    continue;
-                q.push(DATA(move_r, move_c, type));
-                dist[move_r][move_c] = dist[now_r][now_c] + 1;
-            }
-        }
+        ans_cnt+=1;
     }
-    if(dist[final_r][final_c]==0)
-        cout<<"KAKTUS";
-    else
-        cout<<dist[final_r][final_c];
-    #if 1
-    cout<<endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            cout << water_dist[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            cout << dist[i][j] << " ";
-        }
-        cout << endl;
-    }
-    #endif
+    cout<<ans_cnt;
 }
