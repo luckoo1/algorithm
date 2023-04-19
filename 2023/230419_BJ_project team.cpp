c63be1f4b639f47feee1ecf3ce68c67bb5222f04
Luckoo <beobsung1@gmail.com>
2023-04-19 20:13:55 +0900

    SAVE

---
+++ 2023/230419_BJ_project team.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    //freopen("Input.txt", "r", stdin);
+    int N;
+    int num;
+    cin>>N;
+    vector<int> vec;
+    for(int i=0;i<2*N;i++)
+    {
+        cin>>num;
+        vec.push_back(num);
+    }
+
+    sort(vec.begin(),vec.end());
+    int a,b;
+    vector<int> sum_team;
+    for(int i=0;i<N;i++)
+    {
+        a=i;
+        b=(2*N)-1-i;
+        num = vec[b]+vec[a];
+        sum_team.push_back(num);
+    }
+    int ans = *min_element(sum_team.begin(),sum_team.end());
+    cout<<ans;
+}
---
+++ 2023/230419_BJ_초콜릿식사.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int need;
+    cin >> need;
+    int size_choco = 1;
+    while (1)
+    {
+        if (size_choco >= need)
+            break;
+        size_choco = size_choco * 2;
+    }
+    cout<<size_choco<<" ";
+    int n=0;
+    while (1)
+    {
+        if(need % size_choco==0)
+            break;
+        n+=1;
+        size_choco = size_choco/2;
+    }
+    cout<<n;
+   
+}
+
+#if 0
+//나의 허접한 답
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int need;
+    cin >> need;
+    int size_choco = 1;
+    while (1)
+    {
+        if (size_choco >= need)
+            break;
+        size_choco = size_choco * 2;
+    }
+    cout<<size_choco<<" ";
+    map<int, int> hash_map;
+    int cnt_break = 0;
+    while (1)
+    {
+        hash_map[cnt_break] = size_choco;
+        if (size_choco== 1)
+            break;
+        size_choco = size_choco / 2;
+        cnt_break += 1;
+    }
+    int n = 0;
+    while(1){
+
+        if(need % hash_map[n]==0){
+            break;
+        }
+        n++;
+    }
+    cout<<n;
+}
+#endif
